e82cc3bf818ac7e8db097b758a86239c6b9d8c02
masangwoo <masw23@naver.com>
2022-03-16 20:04:22 +0900

    kanban #end

---
+++ kanban/src/TaskList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './assets/css/TaskList.css';
+import Task from './Task';
+
+const TaskList = ({tasks}) => {
+  console.log(tasks)
+  return (
+    <div className={styles.TaskList}>
+          <ul>     
+                  {tasks.map((task)=><Task 
+                                          key={task.no}
+                                          name={task.name} 
+                                          done={task.done}/>)}
+          </ul>
+     </div>
+  )
+}
+
+export default TaskList
