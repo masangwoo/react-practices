1f1af7e58bb6e9cccaa6ce0401b05d55522657fb
masangwoo <masw23@naver.com>
2022-03-25 12:08:02 +0900

    emaillist #3

--- emaillist/backend/src/main/java/com/example/demo/controller/ApiController.java
+++ emaillist/backend/src/main/java/com/example/demo/controller/ApiController.java
@@ -3,11 +3,9 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +14,6 @@ import com.example.demo.repository.EmaillistRepository;
 import com.example.demo.vo.EmaillistVo;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:9999"}, allowedHeaders = "*", allowCredentials="false", methods={RequestMethod.GET})
 public class ApiController {
 	
 	@Autowired
--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -3,12 +3,12 @@ import './assets/scss/App.scss';
 import RegisterForm from './RegisterForm';
 import SearchBar from './SearchBar';
 import Emaillist from './Emaillist';
-import data from './assets/json/data.json';
 
 const App = () => {
   const [emails, setEmails] = useState([]);
 
   useEffect(async () => {
+    try {  
       const response = await fetch('/api', {
         method: 'get',
         headers: {
@@ -18,29 +18,56 @@ const App = () => {
         body: null
       });
 
-      if(!response.ok){
-        console.log("error:",response.status, response.statusText);
-        return;
+      if(!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
       }
 
       const json = await response.json();
 
-      if(json.result !== 'success'){
-        console.log("error:", json.message);
-        return;
+      if(json.result !== 'success') {
+        throw new Error(`${json.result} ${json.message}`);
+      }
+      
+      setEmails(json.data);
+    } catch(err) {
+      console.log(err);      
+    }  
+  }, []);
+
+  const notifyKeywordChange = async function(keyword) {
+    try {  
+      const response = await fetch(`/api?kw=${keyword}`, {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: null
+      });
+
+      if(!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
       }
 
+      const json = await response.json();
+
+      if(json.result !== 'success') {
+        throw new Error(`${json.result} ${json.message}`);
+      }
+      
       setEmails(json.data);
-    }, []);
+    } catch(err) {
+      console.log(err);      
+    }     
+  }
 
-
-  const notifyKeywordChange = function(keyword) {
-    setEmails(data.filter(e => e.firstName.indexOf(keyword) != -1 || e.lastName.indexOf(keyword) != -1 || e.email.indexOf(keyword) != -1));
+  const notifyEmailAdd = function(email) {
+    console.log('post /api', email);
   }
 
   return (
     <div className={'App'}>
-      <RegisterForm />
+      <RegisterForm callback={notifyEmailAdd}/>
       <SearchBar callback={notifyKeywordChange}/>
       <Emaillist emails={emails} />
     </div>
